5df6a12fe91cd7e6230fc7bca703ed67bc5dac73
osoufan_t <osoufan@aidm.ai>
2024-03-25 13:59:54 +0200

    Added code on layers/visualization

---
+++ Exercises/e6lec13layers.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+# Generating two layers
+layer1 = np.random.rand(10,10)
+layer2 = np.random.rand(10,10)
+
+x1 = layer1[:,1]
+y1 = layer1[:,1]
+
+x2 = layer2[:,1]
+y2 = layer2[:,1]
+
+# Generate two plots on top of each other
+
+plt.show()

a03624cd285d64d33a6299cbbd96640c6c7f5432
osoufan_t <osoufan@aidm.ai>
2024-04-17 03:39:56 +0300

    Added code on layers/visualization

---
+++ viz_aesthetics.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+mpg = pd.read_csv("datasets/mpg.csv")
+
+plt.figure(figsize=(12, 6))
+
+print(mpg['class'])
+unique_valus, encoded_labels = np.unique(mpg['class'], return_inverse=True)
+print(encoded_labels)
+#encoded_labels = np.unique(mpg['class'], return_inverse=True)
+#encoded_labels = encoded_labels / 10
+#sns.scatterplot(data=mpg, x='displ', y='hwy', alpha=encoded_labels)
+
+#plt.show()

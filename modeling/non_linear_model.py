ef37477d8d8ec1d915087d7b7b464284d0512ed9
osoufan_t <osoufan@aidm.ai>
2024-04-24 20:24:18 +0300

    Non-linear models

--- modeling/non_linear_model.py
+++ modeling/non_linear_model.py
@@ -10,8 +10,8 @@ sim2 = pd.read_csv("../datasets/sim2.csv")
 # Linear model: lm(y ~ x, data = sim1)
 sim2['x2'] = sim2['x'] ** 2
 sim2['x3'] = sim2['x'] ** 3
-sim2['x4'] = sim2['x'] ** 5
-model1 = smf.ols('y ~ x2', data=sim2).fit()
+sim2['x4'] = sim2['x'] ** 4
+model1 = smf.ols('y ~ x + x2 + x3 + x4', data=sim2).fit()
 
 # ploting
 fig, ax = plt.subplots(figsize=(10, 6))
@@ -21,7 +21,7 @@ ax.scatter(sim2['x'], sim2['y'], s=5, color='blue')
 min = sim2['x'].min()
 max = sim2['x'].max()
 x_range = np.linspace(min, max, 100)
-predictions = model1.predict(exog=dict(x2 = x_range ** 2))
+predictions = model1.predict(exog=dict(x=x_range, x2 = x_range ** 2, x3 = x_range ** 3, x4 = x_range ** 4))
 
 ax.plot(x_range, predictions, color='red')
 
